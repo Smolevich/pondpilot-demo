ae96322356efd46d5ea5b1183f49d0fe2a02a106
Stas <smolevich90@gmail.com>
2025-06-07 17:33:29 +0200

    Fix file loading and update instructions

--- src/components/FileLoader.vue
+++ src/components/FileLoader.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref } from 'vue'
 import * as XLSX from 'xlsx'
 
 const widgetContainer = ref(null)
@@ -18,27 +18,39 @@ async function handleFile(event) {
   const file = event.target.files[0]
   if (!file) return
 
-  const conn = await ensureWidget()
-  const fileName = file.name
-  const ext = fileName.split('.').pop().toLowerCase()
-  const buffer = new Uint8Array(await file.arrayBuffer())
+  try {
+    const conn = await ensureWidget()
+    const fileName = file.name
+    const ext = fileName.split('.').pop().toLowerCase()
+    const buffer = new Uint8Array(await file.arrayBuffer())
 
-  if (ext === 'csv') {
-    await conn.bindings.registerFileBuffer(fileName, buffer)
-    await conn.query(`CREATE OR REPLACE TABLE data AS SELECT * FROM read_csv_auto('${fileName}')`)
-  } else if (ext === 'xlsx' || ext === 'xls') {
-    const workbook = XLSX.read(buffer, { type: 'array' })
-    const csv = XLSX.utils.sheet_to_csv(workbook.Sheets[workbook.SheetNames[0]])
-    const csvName = fileName.replace(/\.[^.]+$/, '.csv')
-    await conn.bindings.registerFileText(csvName, csv)
-    await conn.query(`CREATE OR REPLACE TABLE data AS SELECT * FROM read_csv_auto('${csvName}')`)
-  } else {
-    alert('Unsupported file type: ' + ext)
-    return
+    const escaped = fileName.replace(/'/g, "''")
+
+    if (ext === 'csv') {
+      await conn.bindings.registerFileBuffer(fileName, buffer)
+      await conn.query(`CREATE OR REPLACE TABLE data AS SELECT * FROM read_csv_auto('${escaped}')`)
+    } else if (ext === 'xlsx' || ext === 'xls') {
+      const workbook = XLSX.read(buffer, { type: 'array' })
+      const csv = XLSX.utils.sheet_to_csv(workbook.Sheets[workbook.SheetNames[0]])
+      const csvName = fileName.replace(/\.[^.]+$/, '.csv')
+      await conn.bindings.registerFileText(csvName, csv)
+      const csvEscaped = csvName.replace(/'/g, "''")
+      await conn.query(`CREATE OR REPLACE TABLE data AS SELECT * FROM read_csv_auto('${csvEscaped}')`)
+    } else {
+      alert('Unsupported file type: ' + ext)
+      return
+    }
+
+    widget.editor.querySelector('pre').textContent = 'SELECT * FROM data LIMIT 20;'
+    await widget.run()
+  } catch (err) {
+    console.error(err)
+    if (widget && typeof widget.showError === 'function') {
+      widget.showError(err.message)
+    } else {
+      alert(err.message)
+    }
   }
-
-  widget.editor.querySelector('pre').textContent = 'SELECT * FROM data LIMIT 20;'
-  await widget.run()
 }
 </script>
 
